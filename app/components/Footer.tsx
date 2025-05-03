c38e980508224318de4a622718978885534a830d
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2025-05-03 14:39:42 +0300

    finish

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -12,91 +12,104 @@ export default function Footer() {
             alt="Family Rehab logo"
             className="mb-4"
           />
-          <span className="font-bold text-center text-xl">Family Rehab</span>
+          <span className="font-bold text-xl">Family Rehab</span>
         </div>
-        <div className="flex flex-col text-2xl items-start ">
-          <ul className="space-y-2">
-            <li>
-              <a href="#" className="hover:underline">
-                Головна
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Послуги
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Ціни
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:underline">
-                Блог
-              </a>
-            </li>
-          </ul>
+
+        <div className="flex flex-col justify-between h-[210px] text-xl items-start">
+          <a href="/" className="hover:underline">Головна</a>
+          <a href="/services" className="hover:underline">Послуги</a>
+          <a href="/price" className="hover:underline">Ціни</a>
+          <a href="/blog" className="hover:underline">Блог</a>
         </div>
-        <div className="flex flex-col text-2xl items-start">
+
+        <div className="flex flex-col text-xl items-start space-y-2">
           <p>м. Харків, вул. проїзд Стадіонний, 6/5</p>
-          <p className="mt-2">
-            <a href="tel:+380682101302" className="hover:underline">
-              +380 (68)‑21‑01‑302
-            </a>
-          </p>
-          <p>
-            <a href="tel:+380682101302" className="hover:underline">
-              +380 (68)‑21‑01‑302
-            </a>
-          </p>
-          <div className="flex space-x-4 mt-4">
-            <a href="#" aria-label="WhatsApp">
-              <svg className="w-6 h-6" ></svg>
-            </a>
-            <a href="#" aria-label="Telegram">
-              <svg className="w-6 h-6"></svg>
-            </a>
-            <a href="#" aria-label="Instagram">
-              <svg className="w-6 h-6"></svg>
-            </a>
+          <a href="tel:+380682101302" className="hover:underline">
+            +380 (68)‑21‑01‑302
+          </a>
+          <a href="tel:+380682101302" className="hover:underline">
+            +380 (68)‑21‑01‑302
+          </a>
+
+          <div className="mt-4">
+            <p className="font-medium mb-2">Соцмережі</p>
+            <div className="flex items-center gap-4">
+              <a
+                href="viber://chat?number=+380660101302"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:opacity-70 transition"
+              >
+                <Image
+                  src="/img/footer/viber-white.svg"
+                  alt="Viber"
+                  width={24}
+                  height={24}
+                />
+              </a>
+              <a
+                href="https://t.me/yourchannel"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:opacity-70 transition"
+              >
+                <Image
+                  src="/img/footer/telegram-white.svg"
+                  alt="Telegram"
+                  width={24}
+                  height={24}
+                />
+              </a>
+              <a
+                href="https://instagram.com/yourprofile"
+                target="_blank"
+                rel="noreferrer"
+                className="hover:opacity-70 transition"
+              >
+                <Image
+                  src="/img/footer/insta-white.svg"
+                  alt="Instagram"
+                  width={24}
+                  height={24}
+                />
+              </a>
+            </div>
           </div>
         </div>
+
         <div className="flex flex-col items-start">
-          <h4 className="font-bold mb-4 text-xl">Графік роботи</h4>
-
-          <p className="text-[21px]">Вівторок, четвер: 09:00 – 14:00</p>
-          <p className="text-xl">Субота: 09:00 – 14:00</p>
-
+          <h4 className="font-bold mb-4 text-xl">Графік роботи:</h4>
+          <p className="text-[20px]">Вівторок, четвер: 09:00 до 14:00</p>
+          <p className="text-[20px]">Субота: 09:00 до 14:00</p>
           <p className="mt-6 text-[18px] font-medium">
             Ліцензія №1243 від 17.11.2016 року
           </p>
         </div>
       </div>
-  <div className="mt-8 border-t border-white/50 pt-4 text-center text-sm flex flex-col sm:flex-row items-center justify-center gap-2 text-white">
-    <span className="opacity-80">сайт розроблено:</span>
 
-  <a
-    href="https://instagram.com/natalia_profile"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="flex items-center gap-1 hover:underline"
-  >
-    <Image src='/img/insta.svg' width={20} height={20} alt="insta"/>
-    Наталія Твердохліб
-  </a>
+      <div className="mt-8 border-t border-white/50 pt-4 text-center text-sm flex flex-col sm:flex-row items-center justify-center gap-2 text-white">
+        <span className="opacity-80">сайт розроблено:</span>
 
-  <a
-    href="https://instagram.com/maxim_profile"
-    target="_blank"
-    rel="noopener noreferrer"
-    className="flex items-center gap-1 hover:underline"
-  >
-    <Image src='/img/insta.svg' width={20} height={20} alt="insta"/>
-    Максим Васильченко
-  </a>
-</div>
+        <a
+          href="https://www.behance.net/b5a12831"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-1 hover:underline"
+        >
+          <Image src='/img/insta.svg' width={20} height={20} alt="insta" />
+          Наталія Твердохліб
+        </a>
 
+        <a
+          href="https://www.instagram.com/maks_fullstack/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-1 hover:underline"
+        >
+          <Image src='/img/insta.svg' width={20} height={20} alt="insta" />
+          Максим Васильченко
+        </a>
+      </div>
     </footer>
   );
 }
--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
 import { MapPin, Clock, Phone, } from "lucide-react";
+import Image from "next/image";
 
 export default function ContactsPage() {
   const address = "Україна, м. Харків, вул. проїзд Стадіонний, 6/5";
@@ -35,11 +36,11 @@ export default function ContactsPage() {
       <div className="flex-1 max-w-md">
         <h2 className="text-3xl sm:text-4xl lg:text-5xl mb-10 font-bold uppercase text-[#5DD3D3] text-center lg:text-left">нашi контакти</h2>
         <div className="flex items-start gap-3 mb-5">
-          <MapPin className="w-6 h-6 text-orange-500 flex-shrink-0 mt-1" />
+          <Image src='/img/footer/healthicons_geo-location-24px.svg' width={30} height={30} alt="location" />
           <p className="text-gray-800">{address}</p>
         </div>
-        <div className="flex items-start gap-3 mb-5">
-          <Clock className="w-6 h-6 text-orange-500 flex-shrink-0 mt-1" />
+        <div className="flex items-start gap-3 mb-5 mt-10">
+          <Image src='/img/footer/basil_clock-solid.svg' width={30} height={30} alt="basil_clock" />
           <div>
             <p className="font-medium text-gray-800 mb-1">Центр працює:</p>
             <ul className="text-gray-700 space-y-1">
@@ -52,7 +53,7 @@ export default function ContactsPage() {
           </div>
         </div>
         <div className="flex items-start gap-3 mb-5">
-          <Phone className="w-6 h-6 text-orange-500 flex-shrink-0 mt-1" />
+          <Image src='/img/footer/phone.svg' width={30} height={30} alt="phone" />
           <ul className="text-gray-800 space-y-1">
             {phones.map((tel, i) => (
               <li key={i}>
@@ -64,28 +65,60 @@ export default function ContactsPage() {
           </ul>
         </div>
         <div className="mt-2">
-          <p className="font-medium text-gray-800 mb-2">Ми у соцмережах:</p>
+          <p className="font-medium mt-20 uppercase text-gray-800 mb-2">Ми у соцмережах:</p>
           <div className="flex items-center space-x-4">
-            {social.map((s, i) => (
-              <a
-                key={i}
-                href={s.link}
-                target="_blank"
-                rel="noreferrer"
-                className="hover:text-cyan-600 transition"
-              >
-                <s.Icon className="w-6 h-6 text-gray-700" />
-              </a>
-            ))}
+            <a
+              href="viber://chat?number=+380660101302"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:opacity-70 transition"
+            >
+              <Image
+                src="/img/footer/basil_viber-outline.svg"
+                alt="viber"
+                width={30}
+                height={30}
+              />
+            </a>
+            <a
+              href="https://t.me/yourchannel"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:opacity-70 transition"
+            >
+              <Image
+                src="/img/footer/ri_telegram-line.svg"
+                alt="telegram"
+                width={30}
+                height={30}
+              />
+            </a>
+            <a
+              href="https://instagram.com/yourprofile"
+              target="_blank"
+              rel="noreferrer"
+              className="hover:opacity-70 transition"
+            >
+              <Image
+                src="/img/footer/ri_instagram-line.svg"
+                alt="instagram"
+                width={30}
+                height={30}
+              />
+            </a>
           </div>
         </div>
       </div>
-      <div className="flex-1 mt-8 lg:mt-0 lg:ml-12 relative overflow-visible">
-        <div className="w-full h-64 lg:h-120 overflow-hidden" style={{display:'flex', justifyContent:'end'}}>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2566.859464326589!2d36.3242445!3d49.9577387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41270a52555de29d%3A0x9c252d6ba7f70f5e!2sStadionnyi%20Passage%2C%206-5%2C%20Kharkiv%2C%20Kharkivs&#39;ka%20oblast%2C%2061000!5e0!3m2!1sen!2sua!4v1746216668458!5m2!1sen!2sua" width="80%" height="100%" style={{border:'none' }} loading="lazy"></iframe>
-        </div>
-      </div>
-    </div>
+      <div className="flex-1 mt-8 lg:mt-0 lg:ml-12 relative w-full overflow-visible">
+  <div className="w-full h-64 lg:h-120 overflow-hidden flex justify-center">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2566.859464326589!2d36.3242445!3d49.9577387!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41270a52555de29d%3A0x9c252d6ba7f70f5e!2sStadionnyi%20Passage%2C%206-5%2C%20Kharkiv%2C%20Kharkivs&#39;ka%20oblast%2C%2061000!5e0!3m2!1sen!2sua!4v1746216668458!5m2!1sen!2sua"
+      className="w-[90%] h-full border-0"
+      loading="lazy"
+    ></iframe>
+  </div>
+</div>
+</div>
   </section>
 
     </>
