c2798fe7e84354d66b9f4f98d6ef4f0060db6bf9
Vasilchenk23 <vasilchenko.maksim374@gmail.com>
2025-05-03 14:13:47 +0300

    fix blog

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -11,36 +11,36 @@ export default async function BlogPostPage({ params }: { params: Promise<{ slug:
 
   return (
     <main className="bg-[#FFF1F3] py-16 px-4 min-h-screen">
-      <div className="max-w-[630px] mx-auto rounded-t-xl overflow-hidden">
-        <div
-          className="relative"
-          style={{
-            backgroundColor: post.labelColor || "#5DD3D3",
-            borderTopLeftRadius: "1rem",
-            borderTopRightRadius: "1rem",
-          }}
-        >
-          <div className="text-white text-center text-sm font-bold uppercase px-4 pt-6 pb-3 min-h-[80px]">
-            <div>{post.label}</div>
-            <div className="">{post.excerpt}</div>
-          </div>
-
-          <div className="relative w-[600px] h-[600px] mx-auto overflow-hidden">
-            <Image
-              src={post.cover}
-              alt={post.title}
-              fill
-              className="object-cover"
-              sizes="(max-width: 768px) 100vw, 600px"
-            />
-          </div>
-         
+    <div className="max-w-[630px] mx-auto rounded-t-xl overflow-visible relative">
+      <div
+        className="relative z-10"
+        style={{
+          backgroundColor: post.labelColor || "#5DD3D3",
+          borderTopLeftRadius: "1rem",
+          borderTopRightRadius: "1rem",
+          height: "600px",
+        }}
+      >
+        <div className="text-white text-center text-lg font-bold uppercase px-10 pt-6 pb-3">
+          <div>{post.label}</div>
+          <div className="text-xl normal-case font-medium">{post.excerpt}</div>
         </div>
-        <div
-          className="text-gray-800 leading-7 px-4 sm:px-6 pb-10 pt-6 prose prose-li:marker:text-[#5DD3D3]"
-          dangerouslySetInnerHTML={{ __html: post.content }}
+      </div>
+      <div className="absolute left-1/2 -translate-x-1/2 top-[120px] w-[90%] h-[600px]  overflow-hidden z-20 shadow-lg">
+        <Image
+          src={post.cover}
+          alt={post.title}
+          fill
+          className="object-cover"
+          sizes="(max-width: 768px) 100vw, 600px"
         />
       </div>
-    </main>
+    </div>
+    <div
+      className="prose w-full max-w-[800px] mx-auto prose-p:my-4 prose-li:marker:text-[#5DD3D3] prose-ul:list-disc prose-ul:pl-5 text-gray-800 text-[18px] px-4 sm:px-6 pb-10 pt-[180px]"
+      dangerouslySetInnerHTML={{ __html: post.content }}
+    />
+  </main>
+  
   );
 }
--- app/components/BlogCard.tsx
+++ app/components/BlogCard.tsx
@@ -14,9 +14,9 @@ export function BlogCard({ slug, title, excerpt, cover, label, labelColor }: Pro
   return (
     <Link href={`/blog/${slug}`} className="block overflow-hidden rounded-t-xl">
     <div className="relative" style={{ backgroundColor: labelColor || "#FF4E8A", borderTopLeftRadius: '12px', borderTopRightRadius: '12px' }}>
-      <div className="text-center text-white text-sm font-bold uppercase px-4 pt-4 pb-2 min-h-[80px]">
+      <div className="text-center text-white text-lg font-bold uppercase px-4 pt-4 pb-2 min-h-[80px]">
         <div>{label}</div>
-        <div className="text-xs normal-case font-medium">{excerpt}</div>
+        <div className="text-xl normal-case font-medium">{excerpt}</div>
       </div>
       <div className="relative px-5 w-full h-[240px]">
         <Image
